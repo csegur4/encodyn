6739bcab54fa25f8427aaf68d475437837814911
Carlos Segura Vidal <cibercarlossv@gmail.com>
2025-04-28 12:38:00 -0400

    public build added

--- vite.config.js
+++ vite.config.js
@@ -8,8 +8,11 @@ export default defineConfig({
             input: [
                 'resources/scss/master-about.scss',
                 'resources/scss/master-branding-agency.scss',
-                'resources/scss/master-contact.css',
-                 'resources/scss/master-common.scss',
+                'resources/scss/master-contact.scss',
+                'resources/scss/master-common.scss',
+                'resources/scss/master-blog-details.scss',
+                'resources/scss/master-services.scss',
+                'resources/scss/master-service-details.scss',
                 'resources/js/app.js'
             ],
             refresh: true,
